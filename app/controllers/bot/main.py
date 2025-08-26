ddc0cc631342a1f4f7e5fe10082dd65d0a59c553
infanasotku <infanasotku@gmail.com>
2025-08-26 17:42:19 +0700

    Add command handler for the start command in the bot

--- app/controllers/bot/main.py
+++ app/controllers/bot/main.py
@@ -1,5 +1,11 @@
-from aiogram.types import BotCommand
+from aiogram.types import BotCommand, Message
+from aiogram.filters import Command
 from aiogram import Router
 
 COMMANDS = [BotCommand(command="start", description="Запускает бота")]
 router = Router(name="main")
+
+
+@router.message(Command("start"))
+async def start_command_handler(message: Message):
+    await message.answer("Hello! I'm your bot.")
