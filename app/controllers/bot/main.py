6114c4976e236267dd824e3f7b88fa3e19d60aa4
infanasotku <infanasotku@gmail.com>
2025-08-26 16:40:49 +0700

    Add bot initialization and command setup with webhook handling

---
+++ app/controllers/bot/main.py
@@ -0,0 +1,3 @@
+from aiogram.types import BotCommand
+
+COMMANDS = [BotCommand(command="start", description="Запускает бота")]
---
+++ app/entrypoints/bot.py
@@ -0,0 +1,80 @@
+from contextlib import asynccontextmanager
+from typing import Annotated
+
+from fastapi import FastAPI, Header
+from aiogram import Bot, Dispatcher
+from aiogram.types import Update
+from aiogram.fsm.storage.redis import RedisStorage
+from aiogram.fsm.storage.base import DefaultKeyBuilder
+
+from app.infra.config import settings
+from app.container import Container
+from app.controllers.bot.main import COMMANDS
+
+
+def create_lifespan(container: Container):
+    async def _maybe_future(fut):
+        if fut is not None:
+            await fut
+
+    @asynccontextmanager
+    async def lifespan(app):
+        await _maybe_future(container.init_resources())
+        bot = container.bot()
+        redis = await container.redis()
+        dp = Dispatcher(
+            storage=RedisStorage(redis=redis, key_builder=DefaultKeyBuilder())
+        )
+
+        await bot.set_webhook(
+            url=settings.bot.url,
+            secret_token=settings.bot.secret,
+            allowed_updates=dp.resolve_used_update_types(),
+            drop_pending_updates=True,
+        )
+        await bot.set_my_commands(COMMANDS)
+        register_message_processor(
+            app, bot=bot, dispatcher=dp, secret=settings.bot.secret
+        )
+        try:
+            yield
+        finally:
+            await _maybe_future(container.shutdown_resources())
+
+    return lifespan
+
+
+def create_app():
+    container = Container()
+    container.config.from_pydantic(settings)
+    container.wire(
+        modules=[
+            "app.controllers.bot.main",
+        ]
+    )
+
+    app = FastAPI(docs_url=None, redoc_url=None, lifespan=create_lifespan(container))
+
+    return app
+
+
+def register_message_processor(
+    app: FastAPI, *, bot: Bot, dispatcher: Dispatcher, secret: str
+):
+    """Register webhook endpoint for telegram bot"""
+
+    @app.post("/process_message")
+    async def _(
+        update: dict,
+        x_telegram_bot_api_secret_token: Annotated[str | None, Header()] = None,
+    ):
+        if x_telegram_bot_api_secret_token != secret:
+            return {"status": "error", "message": "Wrong secret token !"}
+        try:
+            answer = await dispatcher.feed_update(bot=bot, update=Update(**update))
+            return answer
+        except Exception:
+            return
+
+
+app = create_app()
