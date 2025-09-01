315e6d61b0cfcbf66637b31beffa043780ecee71
infanasotku <infanasotku@gmail.com>
2025-09-01 23:38:41 +0700

    Update default Redis port to 6379 in RedisSettings

--- app/infra/config/redis.py
+++ app/infra/config/redis.py
@@ -4,7 +4,7 @@ from pydantic import Field, BaseModel, computed_field
 class RedisSettings(BaseModel):
     password: str | None = Field(default=None)
     host: str = Field(default="127.0.0.1")
-    port: int = Field(default=5672)
+    port: int = Field(default=6379)
     db: int = Field(default=0)
 
     @computed_field
